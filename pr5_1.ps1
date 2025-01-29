bcd0b068fe9e43452b30d22e2d01e2dc9916f142
Kaarel Pehap <kaarel.pehap@voco.ee>
2025-01-29 15:20:50 +0200

    esimene

---
+++ pr5_1.ps1
@@ -0,0 +1,13 @@
+﻿Get-Process | Where{$_.ProcessName -eq 'notepad'} | Select Processname, Id
+
+$processes = Get-Process
+foreach($process in $processes)
+{
+Write-Host "Process : "$process -ForegroundColor Yellow
+if($process.PM / 1024 / 1024 -le 100)
+{
+continue
+}
+Write-Host ('Process ' + $process.Name + ' is using more than 100 MB RAM.') -
+ForegroundColor Green
+}
